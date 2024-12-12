8a8740f4ef3c9f055ee237aa734ca5f1a759980d
Vitor <marthendalnunes@gmail.com>
2024-12-12 17:58:21 -0300

    test: add pooltogether v5 to limit order tests

--- test/fork/ForkTest.t.sol
+++ test/fork/ForkTest.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.23;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { BaseTest } from "../utils/BaseTest.t.sol";
+import { PrizeVault } from "pt-v5-vault/src/PrizeVault.sol";
 import { IPool } from "aave-v3-core/interfaces/IPool.sol";
 
 contract ForkTest is BaseTest {
@@ -10,6 +11,7 @@ contract ForkTest is BaseTest {
 
     IERC20 internal constant USDC = IERC20(0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913);
     IERC20 internal constant AUSDC = IERC20(0x4e65fE4DbA92790696d040ac24Aa414708F5c0AB);
+    PrizeVault internal constant przUSDC = PrizeVault(0x7f5C2b379b88499aC2B997Db583f8079503f25b9);
     IERC20 internal constant WETH = IERC20(0x4200000000000000000000000000000000000006);
 
     IPool internal constant AAVE_POOL = IPool(0xA238Dd80C259a72e81d7e4664a9801593F98d1c5);
